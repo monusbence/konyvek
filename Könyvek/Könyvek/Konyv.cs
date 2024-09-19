0192737ac275757a812dc788794d001de539d73e
monusbence <72020098+monusbence@users.noreply.github.com>
2024-09-19 14:12:45 +0200

    Add files via upload

---
+++ Könyvek/Könyvek/Konyv.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Könyvek
+{
+    internal class Konyv
+    {
+        string cim;
+        string szerzo;
+        int kiadasiEv;
+        int oldalSzam;
+        string mufaj;
+
+        public Konyv(string cim, string szerzo, int kiadasiEv, int oldalSzam, string mufaj)
+        {
+            this.cim = cim;
+            this.szerzo = szerzo;
+            this.kiadasiEv = kiadasiEv;
+            this.oldalSzam = oldalSzam;
+            this.mufaj = mufaj;
+        }
+
+        public string Cim { get => cim; set => cim = value; }
+        public string Szerzo { get => szerzo; set => szerzo = value; }
+        public int KiadasiEv { get => kiadasiEv; set => kiadasiEv = value; }
+        public int OldalSzam { get => oldalSzam; set => oldalSzam = value; }
+        public string Mufaj { get => mufaj; set => mufaj = value; }
+    }
+}
---
+++ Könyvek/Könyvek/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace Könyvek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Konyv> konyvLista = new List<Konyv>();
+            using (StreamReader sr = new StreamReader("konyvek.txt"))
+            {
+                string sor;
+                while (!sr.EndOfStream)
+                {
+                    sor = sr.ReadLine();
+                    string[] adatok = sor.Split(',');
+                    string cim = adatok[0];
+                    string szerzo = adatok[1];
+                    int kiadasiEv = int.Parse(adatok[2]);
+                    int oldalSzam = int.Parse(adatok[3]);
+                    string mufaj = adatok[4];
+
+                    Konyv konyvek = new Konyv(cim, szerzo, kiadasiEv, oldalSzam, mufaj);
+                    konyvLista.Add(konyvek);
+                }
+            }
+
+            Console.Write("Add meg a keresett szerző nevét: ");
+            string keresettSzerzo = Console.ReadLine();
+            KonyvekKereseseSzerzoSzerint(konyvLista, keresettSzerzo);
+
+            Konyv legnagyobbKonyv = LegnagyobbOldalszamuKonyv(konyvLista);
+            Console.WriteLine($"A legnagyobb oldalszámú könyv: {legnagyobbKonyv.Cim}, {legnagyobbKonyv.OldalSzam} oldal.");
+
+            SorbarendezesKiadasiEvSzerint(konyvLista);
+
+            KonyvekCsoportositasaMufajSzerint(konyvLista);
+
+
+        }
+
+        static Konyv LegnagyobbOldalszamuKonyv(List<Konyv> konyvLista)
+        {
+            Konyv legnagyobbKonyv = konyvLista[0];
+
+            foreach (var konyv in konyvLista)
+            {
+                if (konyv.OldalSzam > legnagyobbKonyv.OldalSzam)
+                {
+                    legnagyobbKonyv = konyv;
+                }
+            }
+
+            return legnagyobbKonyv;
+        }
+
+        static void KonyvekKereseseSzerzoSzerint(List<Konyv> konyvLista, string szerzo)
+        {
+            bool vanTalalat = false;
+
+            foreach (var konyv in konyvLista)
+            {
+                if (konyv.Szerzo.ToLower() == szerzo.ToLower())
+                {
+                    Console.WriteLine($"Cím: {konyv.Cim}, Szerző: {konyv.Szerzo}, Kiadási év: {konyv.KiadasiEv}, Oldalszám: {konyv.OldalSzam}, Műfaj: {konyv.Mufaj}");
+                    vanTalalat = true;
+                }
+            }
+
+            if (!vanTalalat)
+            {
+                Console.WriteLine($"Nincs találat a(z) {szerzo} nevű szerzőre.");
+            }
+        }
+
+        static void SorbarendezesKiadasiEvSzerint(List<Konyv> konyvLista)
+        {
+            var rendezettLista = konyvLista.OrderBy(konyv => konyv.KiadasiEv).ToList();
+
+            Console.WriteLine("Könyvek kiadási év szerinti növekvő sorrendben:");
+            foreach (var konyv in rendezettLista)
+            {
+                Console.WriteLine($"Cím: {konyv.Cim}, Szerző: {konyv.Szerzo}, Kiadási év: {konyv.KiadasiEv}, Oldalszám: {konyv.OldalSzam}, Műfaj: {konyv.Mufaj}");
+            }
+        }
+
+        static void KonyvekCsoportositasaMufajSzerint(List<Konyv> konyvLista)
+        {
+            var mufajCsoportok = konyvLista.GroupBy(konyv => konyv.Mufaj);
+
+            foreach (var csoport in mufajCsoportok)
+            {
+                Console.WriteLine($"Műfaj: {csoport.Key}, Könyvek száma: {csoport.Count()}");
+
+                foreach (var konyv in csoport)
+                {
+                    Console.WriteLine($"  Cím: {konyv.Cim}, Szerző: {konyv.Szerzo}, Kiadási év: {konyv.KiadasiEv}, Oldalszám: {konyv.OldalSzam}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
